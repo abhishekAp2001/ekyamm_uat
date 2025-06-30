9908b16135a8ba924c3b95b241f220063cb1194f
Manvendra9268 <manvendrapant7011@outlook.com>
2025-06-30 13:31:42 +0530

    dynamic client testimonials added

--- components/patient/Client_Testimonials/Client_Testimonial.js
+++ components/patient/Client_Testimonials/Client_Testimonial.js
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/accordion";
 import { ChevronLeft } from "lucide-react";
 
-const Client_Testimonial = ({ setShowClientTestimonials }) => {
+const Client_Testimonial = ({ setShowClientTestimonials, doc }) => {
   return (
     <div className="bg-gradient-to-t from-[#fce8e5] to-[#eeecfb] min-h-screen w-full px-4 max-w-[576px] mx-auto">
       <div className="w-full fixed top left-0 right-0 z-10 max-w-[576px] mx-auto bg-[#f0ecf9]">
@@ -24,35 +24,14 @@ const Client_Testimonial = ({ setShowClientTestimonials }) => {
           </span>
         </div>
       </div>
-      <div className="pt-[16%] lg:pt-[10%]">
+      {doc?.testimonials?.map((testimonial, index) => (
+
+      <div className="pt-[16%] lg:pt-[10%]" key={index}>        
         <Accordion
           type="single"
           collapsible
           className="w-full flex flex-col gap-3"
         >
-          <AccordionItem
-            value="item-1"
-            className="rounded-[12px] bg-white px-4 py-3"
-          >
-            <AccordionTrigger className="flex items-start justify-between gap-2 [&>svg]:h-5 [&>svg]:w-5 [&>svg]:text-muted-foreground">
-              <div className="flex flex-col items-start">
-                <div className="flex items-center gap-2">
-                  <p className="text-sm font-semibold text-black">
-                    Priya Kapoor
-                  </p>
-                  <span className="text-[10px] font-medium bg-[#fce8e5] text-black px-2 py-[2px] rounded-[8px]">
-                    Verified
-                  </span>
-                </div>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent className="pb-0">
-              <p className="text-sm text-black text-left mt-2 leading-snug font-normal">
-                Lorem Ipsum whatsa asjuda Lorem Ipsum whatsa asjudaLorem Ipsum
-                whatsa asjudaLorem Ipsum whatsa asjudaLorem Ipsum whatsa asjuda
-              </p>
-            </AccordionContent>
-          </AccordionItem>
 
           <AccordionItem
             value="item-2"
@@ -62,23 +41,24 @@ const Client_Testimonial = ({ setShowClientTestimonials }) => {
               <div className="flex flex-col items-start">
                 <div className="flex items-center gap-2">
                   <p className="text-sm font-semibold text-black">
-                    Nexus Dsliva
+                    {testimonial.patientName}
                   </p>
                   <span className="text-[10px] font-medium bg-[#fce8e5] text-black px-2 py-[2px] rounded-[8px]">
-                    Verified
+                    {testimonial.verified? "Verified" : "Unverified"}
                   </span>
                 </div>
               </div>
             </AccordionTrigger>
             <AccordionContent>
               <p className="text-sm text-black text-left mt-2 leading-snug font-normal">
-                Lorem Ipsum whatsa asjuda Lorem Ipsum whatsa asjudaLorem Ipsum
-                whatsa asjudaLorem Ipsum whatsa asjudaLorem Ipsum whatsa asjuda
+                {testimonial.feedback}
               </p>
             </AccordionContent>
           </AccordionItem>
         </Accordion>
       </div>
+      ))}
+
     </div>
   );
 };
