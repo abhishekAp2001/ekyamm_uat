3bc36c9bdb2704fdbb9d7eba5dd7488b5bebc77b
Manvendra9268 <manvendrapant7011@outlook.com>
2025-07-04 16:10:51 +0530

    edit profile and image uploading complete

---
+++ app/patient/edit-profile/page.js
@@ -0,0 +1,12 @@
+import Edit_Patient from '@/components/patient/Edit_Patient/Edit_Patient'
+import React from 'react'
+
+const page = () => {
+  return (
+
+    <Edit_Patient/>
+    
+  )
+}
+
+export default page
