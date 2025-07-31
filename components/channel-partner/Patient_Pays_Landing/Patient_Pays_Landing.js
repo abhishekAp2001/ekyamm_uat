ca1d7a5caae34e762bd8a3f3e28563e1a6df6bb7
abhishekAp2001 <147477995+abhishekAp2001@users.noreply.github.com>
2025-07-31 14:16:46 +0530

    minor_ui

--- components/channel-partner/Patient_Pays_Landing/Patient_Pays_Landing.js
+++ components/channel-partner/Patient_Pays_Landing/Patient_Pays_Landing.js
@@ -67,26 +67,26 @@ const Patient_Pays_Landing = ({ type }) => {
         src="/images/relaxed-feminine-woman-left.png"
         width={100}
         height={100}
-        className="m-[312px]:w-[312px] min-[358px]:w-[314px] min-[389px]:w-full  md:w-[358px] h-auto mix-blend-multiply absolute bottom-0 right-0"
+        className="m-[312px]:w-[312px] min-[358px]:w-[224px] min-[389px]:w-full  md:w-[358px] h-full mix-blend-multiply absolute bottom-0 right-0"
         alt="ekyamm"
       />
-      <div className="container mx-auto px-3 py-8 relative z-10">
+      <div className="container mx-auto px-3 pt-[34px] pb-8 relative z-10">
         <div className="grid gap-8 lg:gap-16 items-center">
           {/* Content Section */}
           <div className="space-y-6 lg:space-y-8">
-            <div className="space-y-4">
-              <div className="flex flex-col">
-                <div className="">
-                  <strong className="text-[34px] text-[#776EA5] font-bold leading-[34px]">
+            <div className="space-y-4 mb-[40px] md:mb-5">
+              <div className="flex flex-col mb-[30px] md:mb-[26px]">
+                <div className="md:mt-4">
+                  <strong className="text-[34px] md:text-[38px] text-[#776EA5] font-bold leading-[34px]">
                     Balance Your{" "}
-                  </strong>
-                  <strong className="text-[34px] text-[#CC627B] font-bold leading-[34px]">
+                  <strong className="text-[34px] md:text-[38px] text-[#CC627B] font-bold leading-[34px]">
                       Mind
-                    <strong className="text-[34px] text-[#776EA5] font-bold px-[10px]">
+                    <strong className="text-[34px] md:text-[38px]  text-[#776EA5] font-bold px-[10px]">
                       &
                     </strong>
                     Body
                     </strong>
+                  </strong>
                     {/* <strong className="text-[34px] text-[#CC627B] font-bold leading-[34px]">
                       Body
                     </strong> */}
@@ -107,48 +107,27 @@ const Patient_Pays_Landing = ({ type }) => {
                       for
                     </strong>
                   </div> */}
-                  <strong className="text-[34px] text-[#776EA5] font-bold leading-[34px] ps-[10px]">
+                  <br className="block"/>
+                  <strong className="text-[34px] md:text-[38px] text-[#776EA5] font-bold leading-[34px] md:ps-[0px] md:mt-1">
                   for Better Fertility
                   </strong>
                 </div>
               </div>
-              <div className="w-[290px] md:w-[494px] text-black">
-                <p className="text-[16px] font-medium leading-[19px] opacity-80">
-                  Trying to conceive is one of the most emotional journeys
+              <div className="w-[290px] md:w-[494px] text-black mt-7 md:mt-0">
+                <p className="text-[16px] md:text-[20px] font-medium leading-[19px] md:leading-[22px] opacity-80">
+                  Trying to conceive, is one of the most emotional journeys
                   you&apos;ll ever take.
                 </p>
-                <p className="text-[16px] font-medium leading-[19px] mt-4 opacity-80">
-                  The waiting, the hope, the setbacks. It&apos;s a lot to carry.
+                <p className="text-[16px] md:text-[21px] font-bold leading-[19px] md:leading-[22px] mt-6 opacity-80 md:mt-6">
+                  The waiting, the hope, the setbacks. <br className="hidden md:block"/>It&apos;s a lot to carry.
                 </p>
               </div>
             </div>
 
             {/* Feature Points */}
-            <div className="space-y-2">
-              <div className="flex items-center gap-2">
-                <div className="flex-shrink-0 w-6 h-6 bg-[#776EA5] rounded-full flex items-center justify-center">
-                  {/* <Image
-                    src="/images/digital_wellbeing.png"
-                    width={20}
-                    height={20}
-                    className="w-5 h-5 object-cover"
-                    alt="success"
-                  /> */}
-                  <div className="flex flex-col items-center">
-                    <Circle color="white" width={7} height={7} fill="white" />
-                    <Heart color="white" width={10} height={10} fill="white" />
-                  </div>
-                </div>
-                <div className="flex items-center gap-1">
-                  <h3 className="font-semibold text-black text-[14px]">
-                    Emotion -{" "}
-                  </h3>
-                  <p className="text-black text-[14px] opacity-80">
-                    Stay connected & supported
-                  </p>
-                </div>
-              </div>
-              <div className="flex items-center gap-2">
+            <div className="space-y-2 md:mt-8">
+         
+              <div className="flex items-center gap-2 mb-[11px] md:mb-[6px]">
                 <div className="flex-shrink-0 w-6 h-6 bg-[#776EA5] rounded-full flex items-center justify-center">
                   {/* <Image
                     src="/images/network_intelligence.png"
@@ -160,15 +139,15 @@ const Patient_Pays_Landing = ({ type }) => {
                   <Brain color="white" width={16} height={16} />
                 </div>
                 <div className="flex items-center gap-1">
-                  <h3 className="font-semibold text-black text-[14px]">
+                  <h3 className="font-semibold text-black text-[16px] md:text-[20px]">
                     Mind -{" "}
                   </h3>
-                  <p className="text-black text-[14px] opacity-80">
+                  <p className="text-black text-[16px] md:text-[18px] opacity-80">
                     Reduce stress & anxiety
                   </p>
                 </div>
               </div>
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-2 mb-[11px] md:mb-[6px]">
                 <div className="flex-shrink-0 w-6 h-6 bg-[#776EA5] rounded-full flex items-center justify-center">
                   {/* <Image
                     src="/images/self_improvement.png"
@@ -188,26 +167,50 @@ const Patient_Pays_Landing = ({ type }) => {
                     <path d="M32 22c-6.627 0-12 5.373-12 12v4h-4l-8 12h8l8-8v8h8v-8h4v8h8v-8l8 8h8l-8-12h-4v-4c0-6.627-5.373-12-12-12z" />
                   </svg>
                 </div>
-                <div className="flex items-center gap-1">
-                  <h3 className="font-semibold text-black text-[14px]">
+                <div className="flex items-center gap-1 ">
+                  <h3 className="font-semibold text-black text-[16px] md:text-[20px]">
                     Body -{" "}
                   </h3>
-                  <p className="text-black text-[14px] opacity-80">Strength</p>
+                  <p className="text-black text-[16px] md:text-[18px] opacity-80">Strength</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-2 mb-[11px] md:mb-[6px]">
+                <div className="flex-shrink-0 w-6 h-6 bg-[#776EA5] rounded-full flex items-center justify-center">
+                  {/* <Image
+                    src="/images/digital_wellbeing.png"
+                    width={20}
+                    height={20}
+                    className="w-5 h-5 object-cover"
+                    alt="success"
+                  /> */}
+                  <div className="flex flex-col items-center">
+                    <Circle color="white" width={7} height={7} fill="white" />
+                    <Heart color="white" width={10} height={10} fill="white" />
+                  </div>
+                </div>
+                <div className="flex items-center gap-1">
+                  <h3 className="font-semibold text-black text-[16px] md:text-[20px]">
+                    Emotion -{" "}
+                  </h3>
+                  <p className="text-black text-[16px] md:text-[18px] opacity-80">
+                    Stay connected & supported
+                  </p>
                 </div>
               </div>
             </div>
 
             {/* Bottom Section */}
-            <div className="space-y-6 fixed bottom-[48px]">
+            <div className="space-y-6 fixed mt-[40%] md:mt-5">
               <div className="space-y-2">
                 {/* <h2 className="text-[20px] font-bold text-[#776EA5] leading-6">
                   A healthier <br /> mind & body
                   <br /> improves
                   <br /> fertility outcomes.
                 </h2> */}
-                 <h2 className="text-[18px] font-bold text-[#776EA5] leading-6 w-[178px] md:w-[400px]">
-                  A healthier  mind & body
+                 <h2 className="text-[20px] md:text-[22px] font-bold text-[#776EA5] leading-6 md:leading-[30px] w-[200px] md:w-[300px]">
+                 <span className="md:text-[30px] md:leading-[34px]"> A healthier,<br className='pr-2 md:pr-0'/>  mind & body</span><br className="block"/>
                    improves
+                   
                   fertility outcomes.
                 </h2>
               </div>
@@ -223,14 +226,14 @@ const Patient_Pays_Landing = ({ type }) => {
                 Get Support Now
               </Button>
               <div className="flex gap-1 items-center fixed bottom-[26px]">
-                <span className="text-[10px] text-gray-500 font-medium">
+                <span className="text-[10px] md:text-[16px] text-gray-500 font-medium">
                   Powered by
                 </span>
                 <Image
                   src="/images/ekyamm.png"
                   width={77}
                   height={17}
-                  className="w-[77px] mix-blend-multiply"
+                  className="w-[77px] md:w-[86px] mix-blend-multiply"
                   alt="ekyamm"
                 />
               </div>
